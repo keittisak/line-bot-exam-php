b564430b3266fc35576cb4142ed5b0a29e6c2482
keittisak <tay.shangrila@gmail.com>
2018-06-19 17:12:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -34,9 +34,10 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			
-			if($event['source']['groupId'] == "C81fe2451f7fbe94a4d47564386844e04")
+			if($event['source']['groupId'] != "C81fe2451f7fbe94a4d47564386844e04")
 			{
                  //exit();
+				$data['messages'][0]['text'] = $content;
             }
   
             $text = createAnswer($text);
