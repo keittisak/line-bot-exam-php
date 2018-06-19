fb4cab2d28bf7728b9f0ed8c36442a54b13afc0c
keittisak <tay.shangrila@gmail.com>
2018-06-19 17:23:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -43,24 +43,28 @@ if (!is_null($events['events'])) {
 				$text = createAnswer($text);
             			$data['messages'][0]['text'] = $text;
 			}
-				
-  
-            
-            
-            //sleep(2);
-            
-            $post = json_encode($data);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 
-			echo $result . "\r\n";
-		}
+		}else{
+            $data = [
+				'replyToken' => $event['replyToken'],
+				'messages' => [
+                    'type' => 'text',
+                    'text' => $content
+                ],
+			];
+        }
+
+        $post = json_encode($data);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        echo $result . "\r\n";
 	}
 }
 
