3f62077b3a3f1c36c775deab3f2e77f8d7903858
keittisak <tay.shangrila@gmail.com>
2019-04-04 11:02:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -9,14 +9,28 @@ $access_token = 'gqgEkKz8kKUIJ9XwgmBhK3ZbPnzK2W4H6XfBmLMXZ8UJjzmCy9NSzldWU0XFDYK
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-// $hearder = json_encode(getallheaders());
+$hearder = getallheaders();
 
+$hearder['host'] = "bots.dialogflow.com";
+$hearder['content-type'] = 'application/json;charset=UTF-8';
+$url = 'https://bots.dialogflow.com/line/bot-shop-ca827/webhook';
 
+$post = json_encode($events);
 
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
 
+echo $result . "\r\n";
 
+exit();
 
-
+//-----------------------------//
 
 
 
